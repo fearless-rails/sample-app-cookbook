faedb09f769add2ab1a0d77e630f5ee72f5cd476
Zachary Danger Campbell <zacharydangercampbell@gmail.com>
2014-06-17 20:44:54 -0500

    Initial commit
    Generated using "berks cookbook" command

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# 0.1.0
+
+Initial release of sample-app
+
+* Enhancements
+  * an enhancement
+
+* Bug Fixes
+  * a bug fix
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'sample-app'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures sample-app'
+long_description 'Installs/Configures sample-app'
+version          '0.1.0'
+
