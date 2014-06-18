2e034d4e26e5fc9fccd9912c3609bbe90af16a28
Zachary Danger Campbell <zacharydangercampbell@gmail.com>
2014-06-17 21:25:14 -0500

    Create and configure the postgres database

--- metadata.rb
+++ metadata.rb
@@ -6,3 +6,6 @@ description      'Installs/Configures sample-app'
 long_description 'Installs/Configures sample-app'
 version          '0.1.0'
 
+depends 'apt'
+depends 'postgresql'
+depends 'database'
--- recipes/default.rb
+++ recipes/default.rb
@@ -2,7 +2,24 @@
 # Cookbook Name:: sample-app
 # Recipe:: default
 #
-# Copyright (C) 2014 YOUR_NAME
+# Copyright (C) 2014 Zach Campbell
 #
-# All rights reserved - Do Not Redistribute
-#
+
+# runs apt-get update for us, basically
+include_recipe "apt"
+
+# installs the postgresql server
+include_recipe "postgresql::server"
+
+# create the postgresql database for our application
+include_recipe "database::postgresql"
+
+postgresql_database "sample_app_production" do
+  connection(
+    host:     'localhost',
+    port:     5432,
+    username: 'postgres',
+    password: node['postgresql']['password']['postgres']
+  )
+  action :create
+end
